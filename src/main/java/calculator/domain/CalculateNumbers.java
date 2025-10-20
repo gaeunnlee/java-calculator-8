9e7f1d5b58080dc7dc098cbee9e560142f4d8af1
gaeunnlee <gaeunnnleee@gmail.com>
2025-10-20 11:39:49 +0900

    feat: 파싱된 숫자가 음수인 경우 IllegalArgumentException 반환

--- src/main/java/calculator/domain/CalculateNumbers.java
+++ src/main/java/calculator/domain/CalculateNumbers.java
@@ -2,13 +2,15 @@ package calculator.domain;
 
 public class CalculateNumbers {
     public static int getSum(int[] numbers) {
-
         if (numbers.length == 0) {
             return 0;
         }
 
         int sum = 0;
         for (int number : numbers) {
+            if (number < 0) {
+                throw new IllegalArgumentException();
+            }
             sum += number;
         }
         return sum;
